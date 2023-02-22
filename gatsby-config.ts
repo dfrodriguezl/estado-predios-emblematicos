d06a32dcbfda750330fcb75d3ed2e9bdbd128f57
Diego <dfrodriguezl94@gmail.com>
2023-02-22 15:11:39 -0500

    Primer commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
+  pathPrefix: '/estado-predios-emblematicos'
 }
 
 export default config
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import axios from "axios"
 
 const pageStyles = {
   color: "#232129",
@@ -8,182 +9,102 @@ const pageStyles = {
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+  marginBottom: 64
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+const circleStyle: React.CSSProperties = {
+  borderRadius: '50%',
+  width: '50px',
+  height: '50px',
+  background: '#00ff00',
+  border: '3px solid #000',
+  display: 'inline-block',
+  marginRight: '10%',
+  textAlign: 'center'
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
+const textCircleStyle = {
+  margin: 'auto',
+  lineHeight: '50px'
 }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
+const circleStyleRed: React.CSSProperties = {
+  borderRadius: '50%',
+  width: '50px',
+  height: '50px',
+  background: '#ff0000',
+  border: '3px solid #000',
+  display: 'inline-block',
+  marginRight: '10%',
+  textAlign: 'center'
 }
 
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+type Respuesta = {
+  features: Feature[]
 }
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+type Feature = {
+  attributes: Predio
+}
+
+type Predio = {
+  ID_PREDIO: number,
+  NOMBRE_PREDIO: string,
+  NUMERO_ETAPAS: string,
+  NUMERO_ETAPAS_CUMPLIDAS: string
+}
+
+const urlConsultaEstados = "https://geoportal.ant.gov.co/servidor/rest/services/PrediosEmblematicos/Mapa_Predios_Emblem%C3%A1ticos/MapServer/0/query?";
+
+// Check if window is defined (so if in the browser or in node.js).
+const isBrowser = typeof window !== "undefined"
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [numMaxEtapas, setNumMaxEtapas] = React.useState<number[]>([]);
+  const [numEtapasCumplidas, setNumEtapasCumplidas] = React.useState<number>(0);
+  let queryParameters;
+  let id_predio;
+  
+  if (isBrowser) {
+    queryParameters = new URLSearchParams(window.location.search);
+    id_predio = queryParameters.get('id_predio');
+  }
+ 
+
+  const params = {
+    where: "id_predio=" + id_predio,
+    outFields: "id_predio,nombre_predio,numero_etapas,numero_etapas_cumplidas",
+    returnGeometry: false,
+    f: "json"
+  }
+
+  React.useEffect(() => {
+    axios.get<Respuesta>(urlConsultaEstados, {
+      params: params
+    }).then((response) => {
+      setNumMaxEtapas(Array.from(Array(Number(response.data.features[0].attributes.NUMERO_ETAPAS)).keys()))
+      setNumEtapasCumplidas(Number(response.data.features[0].attributes.NUMERO_ETAPAS_CUMPLIDAS));
+    })
+  }, [])
+
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Etapas del proceso
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <br />
+        {numMaxEtapas.map((etapa, i) => {
+          if(i < numEtapasCumplidas ){
+            return (<div style={circleStyle} key={i} >
+              <p style={textCircleStyle}>{etapa+1}</p>
+            </div>)
+          } else {
+            return (<div style={circleStyleRed} key={i} >
+              <p style={textCircleStyle}>{etapa+1}</p>
+            </div>)
+          }  
+        })}
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
     </main>
   )
 }
